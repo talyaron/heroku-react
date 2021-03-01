06beea92f7d266474ee47ef700c60aa7194dd99a
Tal <tal.yaron@gmail.com>
2021-03-01 20:44:12 +0200

    added path

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
 
 
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname+ '/client/build')));
+console.log(__dirname+ '/client/build')
 
 const port = process.env.PORT || 4000;
 app.listen(port, ()=>{
