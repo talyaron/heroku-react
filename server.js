5d2c761ba4b67e35e436f884ecee4d4888b61cf3
Tal <tal.yaron@gmail.com>
2021-03-01 20:37:11 +0200

    fix

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 
-app.use(express.static('./client/build'));
+app.use(express.static('client/build'));
 
 const port = process.env.PORT || 4000;
 app.listen(port, ()=>{
